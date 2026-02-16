08bb8630b6c6cf8e09ade8972823778da315e443
Matthew Miller <cuentaparatodoo0z@gmail.com>
2026-02-16 03:20:34 -0700

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
             .map(([name, count]) => `${name}: ${count}`)
             .join(' / ');
 
-        const responseText = `ACTIVE: ${activos} / TODAY: ${todayExecs} / ALL TIME: ${total} / COUNTRIES: ${countriesFormatted || "NONE"}`;
+        const responseText = `Active: ${activos} / Today: ${todayExecs} / All time: ${total} / Countries: ${countriesFormatted || "NONE"}`;
         
         return res.status(200).send(responseText);
 
